598416c6e0c6818f587706b16ec160e71359fa3b
Henrik Loeser <data-henrik@users.noreply.github.com>
2016-07-07 12:13:39 +0200

    Update henriksDialog.py
    added ability to converse, i.e., to chat with Watson

--- henriksDialog.py
+++ henriksDialog.py
@@ -8,25 +8,30 @@
 # Some of the source is taken from this SDK example:
 # https://github.com/watson-developer-cloud/python-sdk/blob/master/examples/dialog_v1.py
 #
-# Author: Henrik Loeser (data-henrik)
+# Author: Henrik Loeser
 
 import json,argparse
 from os.path import join, dirname
 from watson_developer_cloud import DialogV1
 
+# Credentials are read from a file
 with open("config.json") as confFile:
      config=json.load(confFile)['credentials']
 
+# Initialize the dialog with username & password
 dialog = DialogV1(
     username=config['username'],
     password=config['password'])
 
+# Define parameters that we want to catch and some basic command help
 def getParameters(args=None):
    parser = argparse.ArgumentParser(description='Process my Watson Dialog Commands',prog='henriksDialog')
    parser.add_argument("--register","-r", dest='registerDialog', action='store_true',
                        help='--register -dn diaglogName -f dialogFile')
    parser.add_argument("--list", "-l",dest='listDialog', action='store_true',
                        help='--list')
+   parser.add_argument("--converse", "-c",dest='startDialog', action='store_true',
+                       help='--converse -id dialogID')
    parser.add_argument("--update", "-u",dest='updateDialog', action='store_true',
                        help='--update -id dialogID -f dialogFile')
    parser.add_argument("--delete", "-d",dest='deleteDialog', action='store_true',
@@ -62,6 +67,23 @@ def deleteDialog(dialogID):
   print(json.dumps(dialog.delete_dialog(dialog_id=dialogID), indent=2))
 
 
+# Start a dialog and converse with Watson
+def converse(dialogID):
+  print "Starting a dialog, stop by Ctrl+C"
+  print "=================================\n"
+  # Start the dialog
+  resp = dialog.conversation(dialogID)
+  # Output is printed. It is not pretty, but takes care of all parts...
+  print resp['response']
+  # Now loop to chat
+  while True:
+    dinput = raw_input("Please enter:\n")
+    resp=dialog.conversation(dialog_id=dialogID,
+                             dialog_input=dinput,
+                             conversation_id=resp['conversation_id'],
+                             client_id=resp['client_id'])
+    print resp['response']
+
 #
 # Main program, for now just detect what function to call
 #
@@ -76,4 +98,5 @@ if __name__ == '__main__':
        updateDialog(parms.dialogFile,parms.dialogID)
     if (parms.deleteDialog and parms.dialogID):
        deleteDialog(parms.dialogID)
-
+    if (parms.startDialog and parms.dialogID):
+       converse(parms.dialogID)
